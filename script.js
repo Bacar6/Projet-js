67e746a477198338a49e238654dece0e9bd3554b
gala <andhoumani13003.g@gmail.com>
2022-08-27 10:43:25 +0200

    Exo

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-function abracadabra() {
 
-	let prenom =    prompt("Quel est votre prénom ?");
-	let nom    =    prompt("Quel est votre nom ?");
-	let age    =    prompt("Quel est votre age ?");
-
-	alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, " + prenom
-	+ " ! Euh... je veux dire... Monsieur le grand magicien "
-	+ nom
-	+ " ! Cela fait déjà "
-	+ age
-	+ " ans que vous faites raynner notre contrée !");
+function demanderHeure() {
 	
+	let heure = 11;
 
+	if(true) {
+		console.log("Je suis dans la condition....!");
+	}
+	
 }
 
-abracadabra();
 
-
-
-
-
-
-
-
-
+demanderHeure();
 
 
 
