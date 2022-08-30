bdb5ea31daad1e90ba0f0cefe5472106bd2c02a7
gala <andhoumani13003.g@gmail.com>
2022-08-30 11:21:50 +0200

    Exos

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-function demanderHeure() {
-	
-	let heure = 11;
-
-	if(true) {
-		console.log("Je suis dans la condition....!");
-	}
-	
-}
 
 
 
@@ -15,25 +6,8 @@ function demanderHeure() {
 
 
 
-function noteScolaire() {
-
-	let note = 10;
-
-	if(note > 15) {
-		console.log("Très Bien !!!");
-
-	} else if(note => 10) {
-		console.log("Bien !!!");
-		
-	} else {
-		console.log("Insufisant !!!");
-		
-	}
 
 
-}
-
-noteScolaire(20);
 
 
 
